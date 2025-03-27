3049903fd49dbd3ecf62e1b8db9ffa4acba31c2d
Domorozik <danila.k2010@yandex.ru>
2025-03-27 14:30:49 +0300

    Now it gets id for new tasks

--- TaskTracker.py
+++ TaskTracker.py
@@ -1,27 +1,9 @@
 import json
 import os
 import datetime
-from pathlib import Path
 
-def check_saves_exist(file_name = 'saves.json', default_data: dict = None) -> bool:
-    if default_data is None:
-        default_data = {"saved_tasks" : []}
 
-        file_path = Path(file_name)
 
-        if not file_path.exists():
-            try:
-                with open(file_path, 'w', encoding='utf-8') as file:
-                    json.dump(default_data, file, indent=4, ensure_ascii=False)
-                print(f"Файл для сохранения задач {file_name} создан в {file_name}")
-                file.close()
-                return True
-            except Exception as exc:
-                print(f"Ошибка при создании файла:\n{exc}")
-                raise
-        else:
-            print(f"Файл {file_name} уже сущетсвует")
-            return False
 
 if __name__ == "__main__":
     check_saves_exist()
--- Tasks.py
+++ Tasks.py
@@ -1,14 +1,42 @@
 import json
+from pathlib import Path
+
+def check_saves_exist(file_name = 'saves.json', default_data: dict = None) -> bool:
+    if default_data is None:
+        default_data = {"saved_tasks" : []}
+
+        file_path = Path(file_name)
+
+        if not file_path.exists():
+            try:
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    json.dump(default_data, file, indent=4, ensure_ascii=False)
+                print(f"Файл для сохранения задач {file_name} создан в {file_name}")
+                file.close()
+                return True
+            except Exception as exc:
+                print(f"Ошибка при создании файла:\n{exc}")
+                raise
+        else:
+            return False
 
 def get_id():
+    check_saves_exist()
     with open("saves.json", "r") as file:
         saves = json.load(file)
         list_of_ids = []
-        for task in saves["saved_tasks"]:
-            list_of_ids.append(task.get("id"))
-        return list_of_ids
 
-print(get_id())
+    set_of_ids = {task.get("id") for task in saves["saved_tasks"]}
 
-def create_new_task(id):
-    id = 3
+    new_id = 0
+
+    while new_id in set_of_ids:
+        new_id += 1
+    
+    return new_id
+
+def create_new_task():
+    id = get_id()
+    print(id)
+
+create_new_task()
